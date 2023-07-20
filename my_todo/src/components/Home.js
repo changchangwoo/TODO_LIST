98cf20c79f314398a22e0eefc392a24636752294
changchangwoo <changchangwoo@gmail.com>
2023-07-20 15:19:50 +0900

    'base'

--- my_todo/src/components/Home.js
+++ my_todo/src/components/Home.js
@@ -2,19 +2,31 @@ import React, { useState, useEffect } from 'react';
 
 function Home() {
     const [currentTime, setCurrentTime] = useState(new Date());
-
+    const MonthDay = {
+        month: 'long',
+        day: 'numeric'
+    }
+    const HourMinute = {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+    }
+    const HourMinuteTime = currentTime.toLocaleString('en-US', HourMinute);
+    const MonthDayTime = currentTime.toLocaleString('en-US', MonthDay);
     useEffect(() => {
-        const timer = setInterval(() => {
+        setInterval(() => {
             setCurrentTime(new Date());
         }, 1000);
-        return () => {
-            clearInterval(timer);
-        };
     }, []);
 
     return (
         <div className="Home">
-            <div className='Home_time'> {currentTime.toLocaleTimeString()}</div>
+            <div className='Home_MonthDay'>{MonthDayTime}</div>
+            <div className='Home_time'> {HourMinuteTime}</div>
+            <div className='Home_textform'>
+
+            </div>
         </div>
     );
 };
